9f05ccde45eb145dad8c9568cd7030d73158242f
AiratY <drago131100@mail.ru>
2022-11-10 18:51:11 +0400

    Удалил не нужный код

--- app/src/main/java/ru/yunusov/weatherapplication/data/model/ForecastList.kt
+++ app/src/main/java/ru/yunusov/weatherapplication/data/model/ForecastList.kt
@@ -6,18 +6,3 @@ data class ForecastList(
     var list: List<Forecast>,
     val city: City
 )
-
-/*@Entity
-data class ForecastListRoom(
-    @PrimaryKey(autoGenerate = true) val id: Int,
-    val cod: Long,
-    var list: Forecasts
-)
-
-fun fromForecastList(forecastList: ForecastList): ForecastListRoom {
-    return ForecastListRoom(forecastList.id, forecastList.cod, Forecasts(forecastList.list))
-}
-
-fun forecastListRoomToForecastList(forecastListRoom: ForecastListRoom): ForecastList {
-    return ForecastList(forecastListRoom.id, forecastListRoom.cod, forecastListRoom.list.list)
-}*/
